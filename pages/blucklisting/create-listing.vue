86105710801dca420e86f50cf18d8f26f8500aa8
Chandan-webdv <chandankumar07@gmail.com>
2023-12-07 14:58:35 +0530

    new things

---
+++ pages/blucklisting/create-listing.vue
@@ -0,0 +1,51 @@
+<script setup>
+    definePageMeta({
+        layout: "officework",
+    });
+</script>
+
+<template>
+    <div>
+        <!-- nav section start here -->
+        <div class="flex justify-between px-4 py-1 bg-white shadow-md">
+            <div class="flex gap-5 " >
+                <div>
+                <a href="" >
+                    <img src="~assets/images/listing-images/gintaa-icon.png" alt="">
+                    </a>
+                </div>
+            <div>
+                <a href="" >
+                    <img src="~assets/images/listing-images/less-then.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="flex gap-3 pt-2" >    
+            <div>
+                <a href="" class="">
+                    <img src="~assets/images/listing-images/notification.svg" alt="">
+                    </a>
+                </div>
+            <div>
+                <a href="" class="" >
+                    <div class="rounded-full h-8 w-8 bg-blue-500"></div>
+                    </a>
+            </div>    
+            </div>       
+        </div>
+        <!-- nav section end here -->
+
+        <!-- Dashbord  section start here -->
+        <div class="flex">
+            <div class="w-[300px] bg-white shadow-md h-full">
+                
+            </div>
+            <div></div>
+        </div>
+        <!-- Dashbord  section end here -->
+    </div>
+</template>
+
+<style scoped>
+
+</style>
